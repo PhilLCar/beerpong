8e35c79b9564219f1fc608547c3b193c0b4f780b
Philippe Caron <philippe-caron@hotmail.com>
2020-10-15 23:26:20 -0400

    Avancement du webserver socket

--- html/js/tron.js
+++ html/js/tron.js
@@ -460,3 +460,39 @@ function die(next) {
   }
   document.getElementById("T" + next.i + "L" + next.j).setAttribute("color", _COLORS[next.id + 5]);
 }
+
+// SOCKETS
+////////////////////////////////////////////////////////////////////////////
+var socket = null;
+try {
+    // Connexion vers un serveur HTTP
+    // prennant en charge le protocole WebSocket ("ws://").
+    socket = new WebSocket("ws://localhost:8000");
+} catch (exception) {
+    console.error(exception);
+}
+
+// Récupération des erreurs.
+// Si la connexion ne s'établie pas,
+// l'erreur sera émise ici.
+socket.onerror = function(error) {
+    console.error(error);
+};
+
+// Lorsque la connexion est établie.
+socket.onopen = function(event) {
+    console.log("Connexion établie.");
+
+    // Lorsque la connexion se termine.
+    this.onclose = function(event) {
+        console.log("Connexion terminé.");
+    };
+
+    // Lorsque le serveur envoi un message.
+    this.onmessage = function(event) {
+        console.log("Message:", event.data);
+    };
+
+    // Envoi d'un message vers le serveur.
+    this.send("Hello world!");
+};
